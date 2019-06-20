14131d2d140bad495e643e1829723ec099b59f5d
Joseph-H-Green-MJG <thecaliman@gmail.com>
2019-06-19 19:43:03 -0700

    added grid to the portfolio items

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,16 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function(props) {
-  // Data that we'll need:
-  // - background image: thumb_image_url
-  // - logo
-  // - description: description
-  // - id: id
-
   const { id, description, thumb_image_url, logo } = props.item;
   return (
-    <div>
-      <img src={thumb_image_url} />
+    <div className="portfolio-item-wrapper">
+      {/* <img src={thumb_image_url} /> */}
       <img src={logo} />
       <div>{description}</div>
       <Link to={`/portfolio/${id}`}>Link</Link>
